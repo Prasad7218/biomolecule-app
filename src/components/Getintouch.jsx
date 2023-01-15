4ac0fa783699a174d55f08b4e58d3f04264501c0
Prasad7218 <yerawarprasad18@gmail.com>
2023-01-15 18:50:27 +0530

    style-added-form

--- src/components/Getintouch.jsx
+++ src/components/Getintouch.jsx
@@ -4,40 +4,42 @@ const Getintouch = () => {
   return (
     <>
       <h1>Get in touch</h1>
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Affilation</label>
-          <input type="text" class="form-control" />
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Comment</label>
-          <input type="text" class="form-control" />
-        </div>
+      <div className="container" style={{ "margin-top": "40px" }}>
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Affilation</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Comment</label>
+            <input type="text" class="form-control" />
+          </div>
 
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
